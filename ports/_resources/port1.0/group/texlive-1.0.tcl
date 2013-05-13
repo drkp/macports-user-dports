58438205dc068ab92f99cba7504b4055e2e92fdf
Dan Ports <dports@macports.org>
2013-05-13 06:02:52 +0000

    texlive portgroup: update master_sites
    git-svn-id: https://svn.macports.org/repository/macports/users/dports@106003 d073be05-634f-4543-b044-5fe20cf6d1d6

--- ports/_resources/port1.0/group/texlive-1.0.tcl
+++ ports/_resources/port1.0/group/texlive-1.0.tcl
@@ -163,7 +163,8 @@ proc texlive.texmfport {} {
     supported_archs noarch
     installs_libs   no
 
-    master_sites    http://flute.csail.mit.edu/texlive/test/
+    master_sites    http://theremin.csail.mit.edu/texlive/test/ \
+                    http://flute.csail.mit.edu/texlive/test/
     use_xz          yes
 
     global name master_sites distname extract.suffix
